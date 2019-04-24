60ca8735c2242b9c699ca806f76fd07a3cc374fa
Felipe Lima Freire <felipe.lima.freire@concrete.com.br>
2019-04-24 15:26:31 -0300

    Adds SAP class for testing

--- src/main/java/br/com/tdd/mocks/GeradorDeNotaFiscal.java
+++ src/main/java/br/com/tdd/mocks/GeradorDeNotaFiscal.java
@@ -5,15 +5,19 @@ import java.util.Calendar;
 public class GeradorDeNotaFiscal {
 
     private NFDao dao;
+    private SAP sap;
 
-    public GeradorDeNotaFiscal(NFDao dao) {
+    public GeradorDeNotaFiscal(NFDao dao, SAP sap) {
         this.dao = dao;
+        this.sap = sap;
     }
 
     public NotaFiscal gera(Pedido pedido) {
         NotaFiscal nf = new NotaFiscal(pedido.getCliente(), pedido.getValorTotal() * 0.94, Calendar.getInstance());
 
         this.dao.persiste(nf);
+        this.sap.envia(nf);
+
         return nf;
     }
 }
--- src/test/java/br/com/tdd/mocks/GeradorDeNotaFiscalTest.java
+++ src/test/java/br/com/tdd/mocks/GeradorDeNotaFiscalTest.java
@@ -9,7 +9,7 @@ class GeradorDeNotaFiscalTest {
 
     @Test
     public void deveGerarNFComValorDeImpostoDescontado() {
-        GeradorDeNotaFiscal gerador = new GeradorDeNotaFiscal(new NFDao());
+        GeradorDeNotaFiscal gerador = new GeradorDeNotaFiscal(new NFDao(), new SAP());
         Pedido pedido = new Pedido("Mauricio", 1000, 1);
         NotaFiscal nf = gerador.gera(pedido);
         assertEquals(1000 * 0.94, nf.getValor(), 0.0001);
@@ -18,7 +18,8 @@ class GeradorDeNotaFiscalTest {
     @Test
     public void devePersistirNFGerada() { // criando o mock
         NFDao dao = Mockito.mock(NFDao.class);
-        GeradorDeNotaFiscal gerador = new GeradorDeNotaFiscal(dao);
+        final SAP sap = Mockito.mock(SAP.class);
+        GeradorDeNotaFiscal gerador = new GeradorDeNotaFiscal(dao, sap);
         Pedido pedido = new Pedido("Mauricio", 1000, 1);
         NotaFiscal nf = gerador.gera(pedido);
 
@@ -26,4 +27,17 @@ class GeradorDeNotaFiscalTest {
         Mockito.verify(dao).persiste(nf);
     }
 
+    @Test
+    public void deveEnviarNFGeradaParaSAP() {
+        final NFDao dao = Mockito.mock(NFDao.class);
+        final SAP sap = Mockito.mock(SAP.class);
+
+        final GeradorDeNotaFiscal gerador = new GeradorDeNotaFiscal(dao, sap);
+
+        final Pedido pedido = new Pedido("Mauricio", 1000, 1);
+
+        final NotaFiscal nf = gerador.gera(pedido);
+
+        Mockito.verify(sap).envia(nf);
+    }
 }
